b932905052353c494e856cbdf4d66ba2023f9c94
SamuelSaylorNEISD <ssaylor9018@stu.neisd.net>
2024-08-13 12:05:38 -0500

    test

---
+++ Main.java
@@ -0,0 +1,10 @@
+import Java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("does it save to github");
+    String ans = scan.nextLine();
+    System.out.println(ans);
+  }
+}
